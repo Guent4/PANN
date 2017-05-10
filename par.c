dbcf04d97d0381f09c3b48ecaee4b995e8aa90be
Aaron Wisner <daw268@cornell.edu>
2017-05-10 03:12:23 -0400

    worked on parallelizing feed forward

--- par.c
+++ par.c
@@ -12,8 +12,12 @@
 #include <unistd.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include "cublas_v2.h"
 
 #include "matrix.h"
+
+#define IDX2C(i,j,ld) (((j)*(ld))+(i))
+#define BILLION 1000000000
 
 #define TOTAL 8200
 
@@ -25,9 +29,12 @@ void backPropagation(Matrix *estimation);
 void readInXY(int starting, int ending, Matrix *inputs, Matrix *outputs);
 void initializeMatrices();
 void freeMatrices();
+uint64_t get_dt(struct timespec *start, struct timespec *end);
 
 void printVector(float *vector, int len);
 
+static cublasHandle_t handle;
+static cublasStatus_t stat;
 
 static int N;
 static int FEATURES;
@@ -60,11 +67,22 @@ int main(int argc, char **argv)
     }
     LAYER_SIZES[NUM_LAYERS - 1] = 1; // This has to be 1
 
+    // init cublas
+    stat = cublasCreate(&handle);
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        printf ("CUBLAS initialization failed\n");
+        exit(1);
+    }
+
     initializeMatrices();
 
     printf("test accuracy\n");
     testAccuracy(testSize);
     // printMatrix(WTS[1]);
+
+    struct timespec start, end; //timestamps
+    uint64_t total_ff = 0;
+    uint64_t total_bp = 0;
 
     for (int outer = 0; outer < 100; outer++) {
 
@@ -72,8 +90,16 @@ int main(int argc, char **argv)
             // Retrieve data from csv
             readInXY(iter*N, iter*N + N, XTS, YTS);
 
+            clock_gettime(CLOCK_MONOTONIC, &start);
             Matrix *out = feedForward(XTS);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            total_ff += get_dt(&start, &end);
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
             backPropagation(out);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            total_bp += get_dt(&start, &end);
 
             // printf("\n\n\n");
             if (iter % 20 == 0) {
@@ -85,6 +111,9 @@ int main(int argc, char **argv)
             freeMatrix(out);
         }
     }
+
+    float rt = (float)(total_bp + total_ff);
+    printf("Feed Forward: %f%%, Back prop %f%%\n", 100*total_ff/rt, 100*total_bp/rt);
 
 
     freeMatrices();
@@ -103,7 +132,7 @@ void readInXY(int starting, int ending, Matrix *inputs, Matrix *outputs)
     int i, j;
 
 
-    FILE* fstream = fopen("./dating/temp.csv", "r");
+    FILE* fstream = fopen("./dating/CleanedAndNoramlizedData.csv", "r");
 
     if (fstream == NULL) {
         printf("\n file opening failed ");
@@ -169,13 +198,49 @@ void testAccuracy(int testSize)
 
 Matrix *feedForward(Matrix *in)
 {
-    Matrix *z = NULL;
+    int wts_max = 0; //find max number of elements
+    int max_cols = in->cols;
     for (int layer = 0; layer < NUM_LAYERS; layer++) {
+        int tmp = WTS[layer]->cols*WTS[layer]->rows;
+        wts_max = ( tmp > wts_max) ? tmp : wts_max;
+
+        max_cols = (WTS[layer]->cols > max_cols) ? WTS[layer]->cols : max_cols;
+    }
+
+    float *dev_wts;
+    float *dev_in;
+    float *dev_z;
+    cudaMalloc((void**)&dev_wts, wts_max*sizeof(float));
+    cudaMalloc((void**)&dev_in, in->rows*max_cols*sizeof(float));
+    cudaMalloc((void**)&dev_z, in->rows*max_cols*sizeof(float));
+
+    float alpha = 1;
+    float beta = 0;
+
+
+    Matrix *z = NULL;
+    for (int layer = 0; layer < NUM_LAYERS-1; layer++) {
+        // this will load in transposed
+/*
+        cublasSetMatrix(in->cols, in->rows, sizeof(float),
+                in->m, in->cols, dev_in, in->cols);
+
+
+        // Load WTS[layer]  transposed
+        cublasSetMatrix(WTS[layer]->cols, WTS[layer]->rows, sizeof(float),
+                WTS[layer]->m, WTS[layer]->cols, dev_wts, WTS[layer]->cols);
+
+
+        cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T,
+            in->rows, WTS[layer]->cols, in->cols, &alpha, dev_in, in->cols,
+            dev_wts, WTS[layer]->cols, &beta, dev_z, in->rows);
+
+*/
+
+
         // Multiply Z with W to get S
         z = matrixMatrixMultiply(in, WTS[layer]);
-
-        // Note that the output perceptrons do not have activation function
-        if (layer == NUM_LAYERS - 1) break;
+        //z = newMatrix(in->rows, WTS[layer]->cols);
 
         // Apply activation function to S to get Z
         matrixElementApply(z, sigmoid);
@@ -187,7 +252,11 @@ Matrix *feedForward(Matrix *in)
         in = z;
     }
 
-    return z;
+    cudaFree (dev_wts);
+    cudaFree (dev_z);
+
+    // feed through last layer
+    return matrixMatrixMultiply(in, WTS[NUM_LAYERS-1]);
 }
 
 
@@ -309,4 +378,11 @@ void printVector(float *vector, int len)
 		printf("%f\n", vector[i]);
 	}
 	printf("------------------------------------\n");
+}
+
+
+
+uint64_t get_dt(struct timespec *start, struct timespec *end)
+{
+    return BILLION*(end->tv_sec - start->tv_sec) + (end->tv_nsec - start->tv_nsec);
 }
